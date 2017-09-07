4e431776b119beb067bed08831ba04d107fa1f08
Roman Kh <rhudor@gmail.com>
2017-09-07 15:18:33 +0300

    Updated API

--- api.py
+++ api.py
@@ -1,3 +1,4 @@
+from flask import request
 from flask_socketio import Namespace
 import numpy as np
 
@@ -8,21 +9,27 @@ class API_Namespace(Namespace):
         print("Namespace created")
 
     def on_connect(self):
+        print("User connected", request, request.sid)
         pass
 
     def on_disconnect(self):
+        print("User disconnected", request, request.sid)
         pass
 
     def on_ECG_GET_LIST(self, data, meta):
-        ecg_list = [dict(id=id, name='ecg'+str(id)) for id in range(8)]
-        self.emit('ECG_GOT_LIST', ecg_list, meta)
+        print("GET LIST", data, meta)
+        ecg_list = [dict(id=id, name='ecg' + str(id)) for id in range(8)]
+        payload = dict(event='ECG_GOT_LIST', data=ecg_list, meta=meta)
+        self.emit('ECG_GOT_LIST', payload)
 
     def on_ECG_GET_ITEM_DATA(self, data, meta):
         signal = np.random.normal(0, 1, size=30)
         data['signal'] = signal
-        self.emit('ECG_GOT_ITEM_DATA', data, meta)
+        payload = dict(event='ECG_GOT_ITEM_DATA', data=data, meta=meta)
+        self.emit('ECG_GOT_ITEM_DATA', payload)
 
     def on_ECG_GET_INFERENCE(self, data, meta):
         inference = np.random.normal(0, 1, size=30)
         data['inference'] = inference
-        self.emit('ECG_GOT_INFERENCE', data, meta)
+        payload = dict(event='ECG_GOT_ITEM_DATA', data=data, meta=meta)
+        self.emit('ECG_GOT_INFERENCE', payload)
