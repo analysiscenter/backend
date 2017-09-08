ae031d04e1d3e28337953b8cd51b83301a44cf0b
Roman Kh <rhudor@gmail.com>
2017-09-08 13:04:52 +0300

    Added CT messages

--- api.py
+++ api.py
@@ -17,28 +17,28 @@ class API_Namespace(Namespace):
         pass
 
     def on_ECG_GET_LIST(self, data, meta):
-        print("GET LIST", data, meta)
+        print("ECG GET LIST", data, meta)
         ecg_list = [dict(id=id, name='ecg' + str(id)) for id in range(8)]
         payload = dict(event='ECG_GOT_LIST', data=ecg_list, meta=meta)
         self.emit('ECG_GOT_LIST', payload)
 
     def on_ECG_GET_ITEM_DATA(self, data, meta):
-        print("GET ITEM DATA", data, meta)
+        print("ECG GET ITEM DATA", data, meta)
         signal = np.random.normal(0, 1, size=30)
         data['signal'] = signal.tolist()
         payload = dict(event='ECG_GOT_ITEM_DATA', data=data, meta=meta)
         self.emit('ECG_GOT_ITEM_DATA', payload)
 
     def on_ECG_GET_INFERENCE(self, data, meta):
-        print("GET INFERENCE", data, meta)
+        print("ECG GET INFERENCE", data, meta)
         inference = np.random.normal(0, 1, size=30)
         data['inference'] = inference.tolist()
         payload = dict(event='ECG_GOT_ITEM_DATA', data=data, meta=meta)
         self.emit('ECG_GOT_INFERENCE', payload)
 
-    def on_СT_GET_LIST(self, data, meta):
+    def on_CT_GET_LIST(self, data, meta):
         print("CT GET LIST", data, meta)
-        ecg_list = [dict(id=id, name='ecg' + str(id)) for id in range(8)]
+        ecg_list = [dict(id=id, name='ct' + str(id)) for id in range(8)]
         payload = dict(event='CT_GOT_LIST', data=ecg_list, meta=meta)
         self.emit('CT_GOT_LIST', payload)
 
@@ -53,5 +53,5 @@ class API_Namespace(Namespace):
         print("CT GET INFERENCE", data, meta)
         inference = np.random.normal(0, 1, size=30)
         data['inference'] = inference.tolist()
-        payload = dict(event='CT_GOT_ITEM_DATA', data=data, meta=meta)
+        payload = dict(event='CT_GOT_INFERENCE', data=data, meta=meta)
         self.emit('CT_GOT_INFERENCE', payload)
