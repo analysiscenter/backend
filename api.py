68549170497106a43573b4aecc1b03d8a2356bc5
Roman Kh <rhudor@gmail.com>
2017-09-08 18:33:59 +0300

    Remove event from payload

--- api.py
+++ api.py
@@ -19,39 +19,39 @@ class API_Namespace(Namespace):
     def on_ECG_GET_LIST(self, data, meta):
         print("ECG GET LIST", data, meta)
         ecg_list = [dict(id=id, name='ecg' + str(id)) for id in range(8)]
-        payload = dict(event='ECG_GOT_LIST', data=ecg_list, meta=meta)
+        payload = dict(data=ecg_list, meta=meta)
         self.emit('ECG_GOT_LIST', payload)
 
     def on_ECG_GET_ITEM_DATA(self, data, meta):
         print("ECG GET ITEM DATA", data, meta)
         signal = np.random.normal(0, 1, size=30)
         data['signal'] = signal.tolist()
-        payload = dict(event='ECG_GOT_ITEM_DATA', data=data, meta=meta)
+        payload = dict(data=data, meta=meta)
         self.emit('ECG_GOT_ITEM_DATA', payload)
 
     def on_ECG_GET_INFERENCE(self, data, meta):
         print("ECG GET INFERENCE", data, meta)
         inference = np.random.normal(0, 1, size=30)
         data['inference'] = inference.tolist()
-        payload = dict(event='ECG_GOT_ITEM_DATA', data=data, meta=meta)
+        payload = dict(data=data, meta=meta)
         self.emit('ECG_GOT_INFERENCE', payload)
 
     def on_CT_GET_LIST(self, data, meta):
         print("CT GET LIST", data, meta)
         ecg_list = [dict(id=id, name='ct' + str(id)) for id in range(8)]
-        payload = dict(event='CT_GOT_LIST', data=ecg_list, meta=meta)
+        payload = dict(data=ecg_list, meta=meta)
         self.emit('CT_GOT_LIST', payload)
 
     def on_CT_GET_ITEM_DATA(self, data, meta):
         print("CT GET ITEM DATA", data, meta)
         signal = np.random.normal(0, 1, size=30)
         data['signal'] = signal.tolist()
-        payload = dict(event='CT_GOT_ITEM_DATA', data=data, meta=meta)
+        payload = dict(data=data, meta=meta)
         self.emit('CT_GOT_ITEM_DATA', payload)
 
     def on_CT_GET_INFERENCE(self, data, meta):
         print("CT GET INFERENCE", data, meta)
         inference = np.random.normal(0, 1, size=30)
         data['inference'] = inference.tolist()
-        payload = dict(event='CT_GOT_INFERENCE', data=data, meta=meta)
+        payload = dict(data=data, meta=meta)
         self.emit('CT_GOT_INFERENCE', payload)
