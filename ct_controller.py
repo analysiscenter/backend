735bd2608300aa2e57e4a1a08d60472e18f7675f
Alexander Koryagin <a.koryagin@analysiscenter.ru>
2017-09-12 12:12:18 +0300

    add interface of ct_controller

--- ct_controller.py
+++ ct_controller.py
@@ -1,22 +1,26 @@
 import os
 import sys
+import numpy as np
 
-from lung_cancer.dataset import Dataset as ds
-from lung_cancer.dataset import FilesIndex
+from lung_cancer.dataset import FilesIndex, Pipeline
 from lung_cancer import CTImagesModels
 
-# path to pretrained unet
-PRETRAINED_UNET = ''
+# model config
+unet_path = '/notebooks/segm/analysis/conf/model_best_unet_tiversky_2017-8-4-21-57.hdf5'
+unet_loss = 'tiversky'
+config = {'loss': unet_loss, 'path': unet_path}
 
-# glob-mask of patients that interest us
-LUNA_DIR = ''
-
-# dir with stored model
-PRETRAINED_UNET_DIR = ''
+# chosen id-nums
+ixs_nums = np.array([714, 297, 299, 672])
+lunaix = FilesIndex(path= '/notebooks/data/MRT/luna/s*/*.mhd', no_ext=True)
+all_ixs = lunaix.create_subset(lunaix.index[[714, 297, 299, 672]])
 
 class CtController:
-	def __init__(self):
-		self.ppl_load_scan = _
-		self.ppl_inference_scan = _
+    def __init__(self):
+        # dict of patients indices
+        self.ct_list = dict(zip([str(i) for i in range(len(all_ixs))], [all_ixs.indices]))
 
-		keras_unet_config = {"keras_unet": PRETRAINED_UNET_DIR}
+        self.ppl_load_scan = _
+        self.ppl_inference_scan = _
+
+        keras_unet_config = {"keras_unet": PRETRAINED_UNET_DIR}
